a56fcc05e6be6dd4e41201e9726990dccadc9785
Freddie Mixell <freddiemixell@Freddies-iMac.local>
2020-03-09 11:23:15 -0400

    settings initial js

---
+++ js/settings.js
@@ -0,0 +1,11 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+
+    var toggleInput = document.querySelector( '#yikes-woo-toggle-content-input' );
+
+    toggleInput.addEventListener( 'click', yikesToggleTheContent );
+
+    function yikesToggleTheContent( event ) {
+        
+    }
+
+} );
