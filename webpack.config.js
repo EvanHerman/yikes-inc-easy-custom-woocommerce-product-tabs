530c306a1a003226bd17eb58f7a0678af81ebcc4
Freddie Mixell <fmixell@gmail.com>
2020-08-18 09:43:55 -0400

    copying paths from grunt file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,21 @@
 
 const env = process.env.NODE_ENV
+const path = require( 'path' );
 
 module.exports = env => {
-  console.log(`🛠️  YIKES Watcher: ${env} Mode using ./webpack/webpack.${env}.js 🛠️`);
-  return require(`./webpack/webpack.${env}.js`);
+  console.log(`🛠️YIKES Watcher: ${env} Mode 🛠️`);
+
+  return {
+    entry: {
+        'repeatable-custom-tabs-settings': './js/repeatable-custom-tabs-settings.js',
+        'repeatable-custom-tabs-shared': './js/repeatable-custom-tabs-shared.js',
+        'repeatable-custom-tabs': './js/repeatable-custom-tabs.js',
+        premium: './js/premium.js',
+        settings: './js/settings.js',
+    },
+    output: {
+        filename: '[name].min.js',
+        path: path.resolve( __dirname, 'dist' )
+    },
+  }
 };
